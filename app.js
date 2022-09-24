57d540545617f1f6b7ae3cb9853d4625d2e8ffbb
guneysrhat <guneysrhat@gmail.com>
2022-09-24 15:34:45 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+const apiKey = "09f64283148ea6369c6273dbf953a101";
+
+const form = document.querySelector("form");
+const formInput = document.querySelector("input");
+const warning = document.querySelector(".warning");
+const resultArea = document.querySelector(".cities");
+
+const currentCity = document.querySelector(".aktuel");
