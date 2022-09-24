cffe03913e5e162c1a78f5c63a8ea5f37312386e
guneysrhat <guneysrhat@gmail.com>
2022-09-24 16:38:19 +0300

    error

--- app.js
+++ app.js
@@ -18,14 +18,14 @@ const getWeatherDate = async () => {
   cityCart.classList.add("city");
   let weatherType = "metric";
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=${apiKey}&units=${weatherType}`;
+  try {
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data);
+    const { main, name, sys, weather } = data;
+    const iconUrl = `https://openweathermap.org/img/wn/${weather[0].icon}@4x.png`;
 
-  const res = await fetch(url);
-  const data = await res.json();
-  console.log(data);
-  const { main, name, sys, weather } = data;
-  const iconUrl = `https://openweathermap.org/img/wn/${weather[0].icon}@4x.png`;
-
-  cityCart.innerHTML = `
+    cityCart.innerHTML = `
     <div class="head">
         <h2 class="cityName"><span>${name}</span><sup>${sys.country}</sup></h2>
         <div class="closeIcon">X</div>
@@ -36,5 +36,8 @@ const getWeatherDate = async () => {
     <img class="cityIcon" src="${iconUrl}">
     <p>${weather[0].description}</p>
   `;
-  resultArea.appendChild(cityCart);
+    resultArea.appendChild(cityCart);
+  } catch (error) {
+    warning.innerHTML = `${input} is not a city ${error.massage}`;
+  }
 };
