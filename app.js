bb184404389d89e75a0429edc8e0eadc794e592f
guneysrhat <guneysrhat@gmail.com>
2022-09-24 16:28:12 +0300

    cityCart

--- app.js
+++ app.js
@@ -8,10 +8,33 @@ const resultArea = document.querySelector(".cities");
 const currentCity = document.querySelector(".aktuel");
 
 form.onsubmit = (e) => {
-  e.preventDefoult();
+  e.preventDefault();
   getWeatherDate();
 };
 
 const getWeatherDate = async () => {
+  let input = formInput.value;
+  const cityCart = document.createElement("div");
+  cityCart.classList.add("city");
+  let weatherType = "metric";
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=${apiKey}&units=${weatherType}`;
+
+  const res = await fetch(url);
+  const data = await res.json();
+  console.log(data);
+  const { main, name, sys, weather } = data;
+  const iconUrl = `https://openweathermap.org/img/wn/${weather[0].icon}@4x.png`;
+
+  cityCart.innerHTML = `
+    <div class="head">
+        <h2 class="cityName"><span>${name}</span><sup>${sys.country}</sup></h2>
+        <div class="closeIcon">X</div>
+    </div>
+
+    <div class="cityTemp">${Math.round(main.temp)}<sup>°C</sup></div>
+
+    <img class="cityIcon" src="${iconUrl}">
+    <p>${weather[0].description}</p>
+  `;
+  resultArea.appendChild(cityCart);
 };
